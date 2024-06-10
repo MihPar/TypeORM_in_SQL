503568d75ddd603eda975fe5a5de27acec05c2cc
MihPar <9496260@gmail.com>
2024-06-11 00:28:01 +0300

    2406110027

--- src/pairQuizGame/useCase/handleCronFirst-use-case.ts
+++ src/pairQuizGame/useCase/handleCronFirst-use-case.ts
@@ -7,7 +7,7 @@ import { ChangeStatusToFinishedCommand } from "./changeStatusToFinished-use-case
 import { QuestionGame } from "../domain/entity.questionGame";
 import { QuestionQueryRepository } from "../../question/infrastructury/questionQueryRepository";
 import { AnswersPlayer } from "../../pairQuizGameProgress/domain/entity.answersPlayer";
-import { AnswerStatusEnum } from "../enum/enumPendingPlayer";
+import { AnswerStatusEnum, GameStatusEnum, StatusGameEnum } from "../enum/enumPendingPlayer";
 import { PairQuezGameQueryRepository } from "../infrastructure/pairQuizGameQueryRepository";
 import { Cron } from '@nestjs/schedule';
 import { PairQuizGameRepository } from '../infrastructure/pairQuizGameRepository';
@@ -36,32 +36,62 @@ export class CronFirstdUseCase implements ICommandHandler<CronFirstCommand> {
 	// }
 // )
 	async execute(command: CronFirstCommand): Promise<any> {
-		setTimeout(async() => {
-			const currentQuestionIndex: number = command.activeUserGame.firstPlayerProgress.answers.length
-			const gameQuestion: QuestionGame = command.game.questionGames.find((q) => q.index === (currentQuestionIndex))
-			const question = await this.questionQueryRepository.getQuestionById(gameQuestion.question.id)
-					const isIncludes = question!.correctAnswers.includes(command.inputAnswer)
-
-					const answer = AnswersPlayer.createAnswer(
-						question!.id,
-						isIncludes ? AnswerStatusEnum.Correct : AnswerStatusEnum.InCorrect,
-						command.inputAnswer,
-						command.game.firstPlayerProgress,
-					);
-					const answerPush = command.game.firstPlayerProgress.answers
-					answerPush.push(answer)
-				await this.pairQuezGameQueryRepository.createAnswers(answerPush)
-				await this.pairQuizGameRepository.sendAnswerPlayer(
-					{
-						userId: command.game.firstPlayerProgress.user.id,
-						count: (isIncludes ? true : false),
-						gameId: command.game.id
-					}
-				)
-				command.game =  await this.pairQuezGameQueryRepository.getUnfinishedGame(command.game.firstPlayerProgress.user.id)
+		const setTimeFirst = setTimeout(async() => {
+			const firstPlayer = await this.pairQuezGameQueryRepository.getGameByUserIdAndStatuses(command.game.id, command.game.firstPlayerProgress.user.id, [GameStatusEnum.Active])
+			if(firstPlayer.firstPlayerProgress.answers.length === 5) {
+				clearTimeout(setTimeFirst) 
 				return await this.pairQuezGameQueryRepository.saveGame(command.game)
+			} else {
+				const currentQuestionIndex: number = command.activeUserGame.firstPlayerProgress.answers.length
+				const gameQuestion: QuestionGame = command.game.questionGames.find((q) => q.index === (currentQuestionIndex))
+				const question = await this.questionQueryRepository.getQuestionById(gameQuestion.question.id)
+						const isIncludes = question!.correctAnswers.includes(command.inputAnswer)
+	
+						const answer = AnswersPlayer.createAnswer(
+							question!.id,
+							isIncludes ? AnswerStatusEnum.Correct : AnswerStatusEnum.InCorrect,
+							command.inputAnswer,
+							command.game.firstPlayerProgress,
+						);
+						const answerPush = command.game.firstPlayerProgress.answers
+						answerPush.push(answer)
+					await this.pairQuezGameQueryRepository.createAnswers(answerPush)
+					await this.pairQuizGameRepository.sendAnswerPlayer(
+						{
+							userId: command.game.firstPlayerProgress.user.id,
+							count: (isIncludes ? true : false),
+							gameId: command.game.id
+						}
+					)
+					command.game =  await this.pairQuezGameQueryRepository.getUnfinishedGame(command.game.firstPlayerProgress.user.id)
+					command.game.firstPlayerProgress.score += 1
+
+					const firstPlayerScore = command.game.firstPlayerProgress.score
+					const secondPalyerScore = command.game.secondPlayerProgress.score
+					if(firstPlayerScore > secondPalyerScore)  {
+						command.game.secondPlayerProgress.userStatus = StatusGameEnum.Loser
+						command.game.firstPlayerProgress.userStatus = StatusGameEnum.Winner
+						// await this.pairQuezGameQueryRepository.makeFirstPlayerWin(command.game)
+					}
+					if(firstPlayerScore < secondPalyerScore) {
+						command.game.secondPlayerProgress.userStatus = StatusGameEnum.Winner
+						command.game.firstPlayerProgress.userStatus = StatusGameEnum.Loser
+						// await this.pairQuezGameQueryRepository.makeSecondPlayerWin(command.game);
+					}
+					if(firstPlayerScore === secondPalyerScore) {
+						command.game.secondPlayerProgress.userStatus = StatusGameEnum.Draw
+						command.game.firstPlayerProgress.userStatus = StatusGameEnum.Draw
+						// await this.pairQuezGameQueryRepository.notAWinner(command.game);
+					}
+					command.game.status = GameStatusEnum.Finished
+					await this.pairQuezGameQueryRepository.saveProgress(command.game.firstPlayerProgress)
+					await this.pairQuezGameQueryRepository.saveProgress(command.game.secondPlayerProgress)
+
+					return await this.pairQuezGameQueryRepository.saveGame(command.game)
+			}
+			
 		}, 10000)
-		command.game.firstPlayerProgress.score += 1
+		// command.game.firstPlayerProgress.score += 1
 		return await this.pairQuezGameQueryRepository.saveGame(command.game)
 	}
 }
--- src/pairQuizGame/useCase/handleCronSecon-use-case.ts
+++ src/pairQuizGame/useCase/handleCronSecon-use-case.ts
@@ -7,7 +7,7 @@ import { ChangeStatusToFinishedCommand } from "./changeStatusToFinished-use-case
 import { QuestionGame } from "../domain/entity.questionGame";
 import { QuestionQueryRepository } from "../../question/infrastructury/questionQueryRepository";
 import { AnswersPlayer } from "../../pairQuizGameProgress/domain/entity.answersPlayer";
-import { AnswerStatusEnum } from "../enum/enumPendingPlayer";
+import { AnswerStatusEnum, GameStatusEnum, StatusGameEnum } from "../enum/enumPendingPlayer";
 import { PairQuezGameQueryRepository } from "../infrastructure/pairQuizGameQueryRepository";
 import { Cron } from '@nestjs/schedule';
 import { sortAddedAt } from '../../helpers/helpers';
@@ -35,33 +35,63 @@ export class CronSecondUseCase implements ICommandHandler<CronSecondCommand> {
 			
 	// })
 	async execute(command: CronSecondCommand): Promise<any> {
-		setTimeout(async() => {
-			const currentQuestionIndex: number = command.activeUserGame.secondPlayerProgress.answers.length
-			const gameQuestion: QuestionGame = command.game.questionGames.find((q) => q.index === (currentQuestionIndex))
-			const question = await this.questionQueryRepository.getQuestionById(gameQuestion.question.id)
-			const isIncludes = question!.correctAnswers.includes(command.inputAnswer)
+		const setTimeSecond = setTimeout(async() => {
+			const secondPlayer = await this.pairQuezGameQueryRepository.getGameByUserIdAndStatuses(command.game.id, command.game.secondPlayerProgress.user.id, [GameStatusEnum.Active])
+			if(secondPlayer.secondPlayerProgress.answers.length === 5) {
+				clearTimeout(setTimeSecond)
+				return await this.pairQuezGameQueryRepository.saveGame(command.game)
+			} else {
+				const currentQuestionIndex: number = command.activeUserGame.secondPlayerProgress.answers.length
+				const gameQuestion: QuestionGame = command.game.questionGames.find((q) => q.index === (currentQuestionIndex))
+				const question = await this.questionQueryRepository.getQuestionById(gameQuestion.question.id)
+				const isIncludes = question!.correctAnswers.includes(command.inputAnswer)
 
-			const answer = AnswersPlayer.createAnswer(
-						question!.id,
-						isIncludes ? AnswerStatusEnum.Correct : AnswerStatusEnum.InCorrect,
-						command.inputAnswer,
-						command.game.secondPlayerProgress,
-					);
-			const answerPush = command.game.secondPlayerProgress.answers
-			answerPush.push(answer)
-			await this.pairQuezGameQueryRepository.createAnswers(answerPush)
+				const answer = AnswersPlayer.createAnswer(
+							question!.id,
+							isIncludes ? AnswerStatusEnum.Correct : AnswerStatusEnum.InCorrect,
+							command.inputAnswer,
+							command.game.secondPlayerProgress,
+						);
+				const answerPush = command.game.secondPlayerProgress.answers
+				answerPush.push(answer)
+				await this.pairQuezGameQueryRepository.createAnswers(answerPush)
 
-			await this.pairQuizGameRepository.sendAnswerPlayer(
-				{
-					userId: command.game.secondPlayerProgress.user.id,
-					count: (isIncludes ? true : false),
-					gameId: command.game.id
+				await this.pairQuizGameRepository.sendAnswerPlayer(
+					{
+						userId: command.game.secondPlayerProgress.user.id,
+						count: (isIncludes ? true : false),
+						gameId: command.game.id
+					}
+				)
+				command.game =  await this.pairQuezGameQueryRepository.getUnfinishedGame(command.game.secondPlayerProgress.user.id)
+				command.game.secondPlayerProgress.score += 1
+				const firstPlayerScore = command.game.firstPlayerProgress.score
+				const secondPalyerScore = command.game.secondPlayerProgress.score
+				if(firstPlayerScore > secondPalyerScore)  {
+					command.game.secondPlayerProgress.userStatus = StatusGameEnum.Loser
+					command.game.firstPlayerProgress.userStatus = StatusGameEnum.Winner
+					// await this.pairQuezGameQueryRepository.makeFirstPlayerWin(command.game)
 				}
-			)
-			command.game =  await this.pairQuezGameQueryRepository.getUnfinishedGame(command.game.secondPlayerProgress.user.id)
-			return await this.pairQuezGameQueryRepository.saveGame(command.game)
+				if(firstPlayerScore < secondPalyerScore) {
+					command.game.secondPlayerProgress.userStatus = StatusGameEnum.Winner
+					command.game.firstPlayerProgress.userStatus = StatusGameEnum.Loser
+					// await this.pairQuezGameQueryRepository.makeSecondPlayerWin(command.game);
+				}
+				if(firstPlayerScore === secondPalyerScore) {
+					command.game.secondPlayerProgress.userStatus = StatusGameEnum.Draw
+					command.game.firstPlayerProgress.userStatus = StatusGameEnum.Draw
+					// await this.pairQuezGameQueryRepository.notAWinner(command.game);
+				}
+				command.game.status = GameStatusEnum.Finished
+				await this.pairQuezGameQueryRepository.saveProgress(command.game.firstPlayerProgress)
+				await this.pairQuezGameQueryRepository.saveProgress(command.game.secondPlayerProgress)
+				
+				return await this.pairQuezGameQueryRepository.saveGame(command.game)
+			}
+			
 		}, 10000)
-		command.game.secondPlayerProgress.score += 1
-		return await this.pairQuezGameQueryRepository.saveGame(command.game)
+		// console.log("setTime: ", setTime)
+		// command.game.secondPlayerProgress.score += 1
+		// return await this.pairQuezGameQueryRepository.saveGame(command.game)
 	}
 }
