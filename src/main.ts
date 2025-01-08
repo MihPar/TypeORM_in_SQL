b3b20d917d8cf9421aa90f01fabefa390f708538
MihPar <9496260@gmail.com>
2025-01-08 17:41:50 +0300

    feat: to applay Linter

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSettings } from './setting';
 import { TelegramAdapter } from './telegramm/adapter/telegram.adapter';
-import ngrok from "ngrok"
-const axios = require('axios')
+import ngrok from 'ngrok';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  appSettings(app)
+  appSettings(app);
   await app.listen(4000);
 
-  const telegramAdapter = await app.resolve<TelegramAdapter>(TelegramAdapter)
-//   const settings = {
-// 	currentAppBaseUrl: 'https://localhost:4000'
-//   }
+  const telegramAdapter = await app.resolve<TelegramAdapter>(TelegramAdapter);
+  //   const settings = {
+  // 	currentAppBaseUrl: 'https://localhost:4000'
+  //   }
   async function connectToNgrok(): Promise<string> {
-	const url = await ngrok.connect(4000)
-	return url
-	}
-	// let baseUrl: any = settings.currentAppBaseUrl
-	// if(true) {
-		const baseUrl = await connectToNgrok()
-	// }
-	await telegramAdapter.setWebhook(baseUrl + '/integrations/telegram/webhook')
-	console.log(baseUrl, " your url")
-
+    const url = await ngrok.connect(4000);
+    return url;
+  }
+  // let baseUrl: any = settings.currentAppBaseUrl
+  // if(true) {
+  const baseUrl = await connectToNgrok();
+  // }
+  await telegramAdapter.setWebhook(baseUrl + '/integrations/telegram/webhook');
+  console.log(baseUrl, ' your url');
 }
 bootstrap();
