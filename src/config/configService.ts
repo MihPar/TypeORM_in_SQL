377d72c78a35e30bc2a46f73f95b82e5e976086d
MihPar <9496260@gmail.com>
2025-01-08 17:49:20 +0300

    fix: to do Linter

--- src/config/configService.ts
+++ src/config/configService.ts
@@ -1,39 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ConfigType } from "./configServiceType";
-
+import { ConfigType } from './configServiceType';
 
 @Injectable()
 export class ApiConfigService {
- constructor(
-	private configService: ConfigService<ConfigType>
- ) {}
+  constructor(private configService: ConfigService<ConfigType>) {}
 
- get JWT_SECRET(): string | undefined {
-	return this.configService.get("JWT_SECRET", {infer: true})
- }
+  get JWT_SECRET(): string | undefined {
+    return this.configService.get('JWT_SECRET', { infer: true });
+  }
 
- get EXPIRED_JWT(): string | undefined {
-	return this.configService.get("EXPIRED_JWT", {infer: true})
- }
+  get EXPIRED_JWT(): string | undefined {
+    return this.configService.get('EXPIRED_JWT', { infer: true });
+  }
 
- get REFRESH_JWT_SECRET(): string | undefined {
-	return this.configService.get("REFRESH_JWT_SECRET", {infer: true})
- }
+  get REFRESH_JWT_SECRET(): string | undefined {
+    return this.configService.get('REFRESH_JWT_SECRET', { infer: true });
+  }
 
- get EXPIRED_REFRESH_JWT(): string |undefined {
-	return this.configService.get("EXPIRED_REFRESH", {infer: true})
- }
+  get EXPIRED_REFRESH_JWT(): string | undefined {
+    return this.configService.get('EXPIRED_REFRESH', { infer: true });
+  }
 
-//  get USERNAME(): string |undefined {
-// 	return this.configService.get("USERNAME", {infer: true})
-//  }
+  //  get USERNAME(): string |undefined {
+  // 	return this.configService.get("USERNAME", {infer: true})
+  //  }
 
-//  get PASSWORD(): string |undefined {
-// 	return this.configService.get("PASSWORD", {infer: true})
-//  }
+  //  get PASSWORD(): string |undefined {
+  // 	return this.configService.get("PASSWORD", {infer: true})
+  //  }
 
-//  get DATABASE_URL(): string |undefined {
-// 	return this.configService.get("DATABASE_URL", {infer: true})
-//  }
+  //  get DATABASE_URL(): string |undefined {
+  // 	return this.configService.get("DATABASE_URL", {infer: true})
+  //  }
 }
